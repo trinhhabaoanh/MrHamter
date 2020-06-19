8298d3a1052c54714bf17cad43bb0eb04c3eabd6
trinhhabaoanh <67160223+trinhhabaoanh@users.noreply.github.com>
2020-06-19 23:13:32 +0700

    KTGKNgMay

---
+++ KTGKNgMay-master/KTGKNgMay/Controllers/TblCategoriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KTGKNgMay.Models;
+
+namespace KTGKNgMay.Controllers
+{
+    public class TblCategoriesController : Controller
+    {
+        private dbNgMayEntities2 db = new dbNgMayEntities2();
+
+        // GET: TblCategories
+        public ActionResult Index()
+        {
+            return View(db.TblCategory.ToList());
+        }
+
+        // GET: TblCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TblCategory tblCategory = db.TblCategory.Find(id);
+            if (tblCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCategory);
+        }
+
+        // GET: TblCategories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TblCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,CategoryName,CategoryNote")] TblCategory tblCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TblCategory.Add(tblCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblCategory);
+        }
+
+        // GET: TblCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TblCategory tblCategory = db.TblCategory.Find(id);
+            if (tblCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCategory);
+        }
+
+        // POST: TblCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName,CategoryNote")] TblCategory tblCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblCategory);
+        }
+
+        // GET: TblCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TblCategory tblCategory = db.TblCategory.Find(id);
+            if (tblCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCategory);
+        }
+
+        // POST: TblCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TblCategory tblCategory = db.TblCategory.Find(id);
+            db.TblCategory.Remove(tblCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ KTGKNgMay-master/KTGKNgMay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KTGKNgMay.MvcApplication" Language="C#" %>
