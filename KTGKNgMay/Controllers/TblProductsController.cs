cd85aa44a2e45a95837f2917296952a151e91d6a
trinhhabaoanh <67160223+trinhhabaoanh@users.noreply.github.com>
2020-06-19 23:06:51 +0700

    Mrhamter

---
+++ KTGKNgMay/Controllers/TblProductsController.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using KTGKNgMay.Models;
+using System.Reflection;
+using System.Linq.Expressions;
+using PagedList;
+using System.Linq.Dynamic;
+
+namespace KTGKNgMay.Controllers
+{
+    public class TblProductsController : Controller
+    {
+        private dbNgMayEntities2 db = new dbNgMayEntities2();
+
+        public class HttpParamActionAttribute : ActionNameSelectorAttribute
+        {
+            public override bool IsValidName(ControllerContext controllerContext, string actionName, MethodInfo methodInfo)
+            {
+                if (actionName.Equals(methodInfo.Name, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+
+                var request = controllerContext.RequestContext.HttpContext.Request;
+                return request[methodInfo.Name] != null;
+            }
+        }
+
+        // GET: TblProducts
+        //public ActionResult Index(string sortProperty, string sortOrder, string searchString)
+        public ActionResult Index(int? size, int? page, string sortProperty, string sortOrder, string searchString)
+        {
+            // 1. Tạo biến ViewBag gồm sortOrder, searchValue, sortProperty và page
+            if (sortOrder == "asc") ViewBag.sortOrder = "desc";
+            if (sortOrder == "desc") ViewBag.sortOrder = "";
+            if (sortOrder == "") ViewBag.sortOrder = "asc";
+            ViewBag.searchValue = searchString;
+            ViewBag.sortProperty = sortProperty;
+            ViewBag.page = page;
+
+            // 2. Tạo danh sách chọn số trang
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "5", Value = "5" });
+            items.Add(new SelectListItem { Text = "10", Value = "10" });
+            items.Add(new SelectListItem { Text = "20", Value = "20" });
+            items.Add(new SelectListItem { Text = "25", Value = "25" });
+            items.Add(new SelectListItem { Text = "50", Value = "50" });
+            items.Add(new SelectListItem { Text = "100", Value = "100" });
+            items.Add(new SelectListItem { Text = "200", Value = "200" });
+
+            // 2.1. Thiết lập số trang đang chọn vào danh sách List<SelectListItem> items
+            foreach (var item in items)
+            {
+                if (item.Value == size.ToString()) item.Selected = true;
+            }
+            ViewBag.size = items;
+            ViewBag.currentSize = size;
+
+            // 3. Lấy tất cả tên thuộc tính của lớp Link (LinkID, LinkName, LinkURL,...)
+            var properties = typeof(TblProduct).GetProperties();
+            List<Tuple<string, bool, int>> list = new List<Tuple<string, bool, int>>();
+            foreach (var item in properties)
+            {
+                int order = 999;
+                var isVirtual = item.GetAccessors()[0].IsVirtual;
+
+                if (item.Name == "Products ID") order = 1;
+                if (item.Name == "Products Code") order = 2;
+                if (item.Name == "Products Name") order = 3;
+                if (item.Name == "Products Short Name") order = 4;
+                if (item.Name == "Products Note") order = 5;
+                if (item.Name == "CategoryID") continue; // Không hiển thị cột này
+                Tuple<string, bool, int> t = new Tuple<string, bool, int>(item.Name, isVirtual, order);
+                list.Add(t);
+            }
+            list = list.OrderBy(x => x.Item3).ToList();
+
+            // 3.1. Tạo Heading sắp xếp cho các cột
+            foreach (var item in list)
+            {
+                if (!item.Item2)
+                {
+                    if (sortOrder == "desc" && sortProperty == item.Item1)
+                    {
+                        ViewBag.Headings += "<th><a href='?page=" + page + "&size=" + ViewBag.currentSize + "&sortProperty=" + item.Item1 + "&sortOrder=" +
+                            ViewBag.sortOrder + "&searchString=" + searchString + "'>" + item.Item1 + "<i class='fa fa-fw fa-sort-desc'></i></th></a></th>";
+                    }
+                    else if (sortOrder == "asc" && sortProperty == item.Item1)
+                    {
+                        ViewBag.Headings += "<th><a href='?page=" + page + "&size=" + ViewBag.currentSize + "&sortProperty=" + item.Item1 + "&sortOrder=" +
+                            ViewBag.sortOrder + "&searchString=" + searchString + "'>" + item.Item1 + "<i class='fa fa-fw fa-sort-asc'></a></th>";
+                    }
+                    else
+                    {
+                        ViewBag.Headings += "<th><a href='?page=" + page + "&size=" + ViewBag.currentSize + "&sortProperty=" + item.Item1 + "&sortOrder=" +
+                           ViewBag.sortOrder + "&searchString=" + searchString + "'>" + item.Item1 + "<i class='fa fa-fw fa-sort'></a></th>";
+                    }
+
+                }
+                else ViewBag.Headings += "<th>" + item.Item1 + "</th>";
+            }
+
+            // 4. Truy vấn lấy tất cả đường dẫn
+            var links = from l in db.TblProduct
+                        select l;
+
+            // 5. Tạo thuộc tính sắp xếp mặc định là "Pro Code"
+            if (String.IsNullOrEmpty(sortProperty)) sortProperty = "ProductCode";
+
+            // 5. Sắp xếp tăng/giảm bằng phương thức OrderBy sử dụng trong thư viện Dynamic LINQ
+            if (sortOrder == "desc") links = links.OrderBy(sortProperty + " desc");
+            else if (sortOrder == "asc") links = links.OrderBy(sortProperty);
+            else links = links.OrderBy("ProductCode");
+
+            // 5.1. Thêm phần tìm kiếm
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                links = links.Where(s => s.ProductName.Contains(searchString));
+            }
+
+            // 5.2. Nếu page = null thì đặt lại là 1.
+            page = page ?? 1; //if (page == null) page = 1;
+
+            // 5.3. Tạo kích thước trang (pageSize), mặc định là 5.
+            int pageSize = (size ?? 5);
+
+            ViewBag.pageSize = pageSize;
+
+            // 6. Toán tử ?? trong C# mô tả nếu page khác null thì lấy giá trị page, còn
+            // nếu page = null thì lấy giá trị 1 cho biến pageNumber. --- dammio.com
+            int pageNumber = (page ?? 1);
+
+            // 6.2 Lấy tổng số record chia cho kích thước để biết bao nhiêu trang
+            int checkTotal = (int)(links.ToList().Count / pageSize) + 1;
+            // Nếu trang vượt qua tổng số trang thì thiết lập là 1 hoặc tổng số trang
+            if (pageNumber > checkTotal) pageNumber = checkTotal;
+
+            // 7. Trả về các Link được phân trang theo kích thước và số trang.
+            return View(links.ToPagedList(pageNumber, pageSize));
+        }
+
+        [HttpPost, HttpParamAction]
+        public ActionResult Reset()
+        {
+            ViewBag.searchValue = "";
+            Index(null, null, "", "", "");
+            return View();
+        }
+
+        // GET: TblProducts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TblProduct tblProduct = db.TblProduct.Find(id);
+            if (tblProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblProduct);
+        }
+
+        // GET: TblProducts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TblProducts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,ProductCode,ProductName,ProductShortName,ProductNote,CategoryID")] TblProduct tblProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TblProduct.Add(tblProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblProduct);
+        }
+
+        // GET: TblProducts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TblProduct tblProduct = db.TblProduct.Find(id);
+            if (tblProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblProduct);
+        }
+
+        // POST: TblProducts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,ProductCode,ProductName,ProductShortName,ProductNote,CategoryID")] TblProduct tblProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblProduct);
+        }
+
+        // GET: TblProducts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TblProduct tblProduct = db.TblProduct.Find(id);
+            if (tblProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblProduct);
+        }
+
+        // POST: TblProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TblProduct tblProduct = db.TblProduct.Find(id);
+            db.TblProduct.Remove(tblProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
